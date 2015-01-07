c908d3eecdc00b9adfe3d06ab953283cbb082ecf
Keraito <dragoon_v2_chak_shun@hotmail.com>
2015-01-07 10:49:48 +0100

    Different scatter plots of frame 1

--- Code/ChakShun.m
+++ Code/ChakShun.m
@@ -1,24 +1,27 @@
-vid = VideoReader('Trainingsvideo.avi');
-frame = read(vid,100); 
-%image(frame);
+vid = VideoReader('C:\Users\ChakShun\Uni\[2014-2015]Q2\Beeldverwerking\Eindproject\Beeldverwerking\Resources\Trainingsvideo.avi');
+frame = read(vid,100);
+image(frame);
 rgeheel = frame(:,:,1); ggeheel = frame(:,:,2); bgeheel = frame(:,:,3);
 
 % Kentekenplaat eruit halen.
-plaat = frame(250:280,335:465,:); 
+%plaat = frame(250:280,335:465,:); 
 %image(plaat);
-red = plaat(:,:,1); green = plaat(:,:,2); blue = plaat(:,:,3);
+%red = plaat(:,:,1); green = plaat(:,:,2); blue = plaat(:,:,3);
 
 % Create Scatterplot
 %figure, hold on, scatter(bgeheel(:),rgeheel(:)), 
 %    scatter(blue(:),red(:),'red'); xlabel('Blauw'); ylabel('Red');
 
  a = dip_image(bgeheel<(160/180)*ggeheel - 20);
- b = label(a,Inf,2000,0);
- 
- I = find(b == 1);
- max(I)
- min(I)
+ b = label(a,Inf,1400,0);
+ c = closing(b,8,'elliptic');
+ %2000
+ result = uint8(frame*c);
+ figure;image(result);
+ %I = find(b == 1);
+ %max(I)
+ %min(I)
 
-I = find(k==1);
-s = [length(k(:,1)) length(k(1,:))];
-[x,y] = ind2sub(s,I);
+%I = find(k==1);
+%s = [length(k(:,1)) length(k(1,:))];
+%[x,y] = ind2sub(s,I); 
