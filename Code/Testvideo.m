7695b8a173938579618680e4cd927315a02a1a51
Keraito <dragoon_v2_chak_shun@hotmail.com>
2015-02-05 16:01:10 +0100

    Multiple kentekens

--- Code/Testvideo.m
+++ Code/Testvideo.m
@@ -5,11 +5,14 @@ tic;
 for(k = 1801)%1:4:vid.NumberofFrames)
 frame=read(vid,k);
 kenteken = run(frame);
-if length(kenteken)>1
-    results{p,1} = kenteken;
-    results{p,2} = k;
-    time = toc;
-    results{p,3} = time; 
-    p = p + 1;
+%if length(kenteken)>1
+for(n = 1:length(kenteken))
+    if(length(kenteken(n)) > 1)
+        results{p,1} = kenteken;
+        results{p,2} = k;
+        time = toc;
+        results{p,3} = time; 
+        p = p + 1;
+    end
 end
 end
