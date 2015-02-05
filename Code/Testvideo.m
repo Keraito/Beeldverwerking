4bd69b20ba991f318fefaee62593a502ed918cda
Keraito <dragoon_v2_chak_shun@hotmail.com>
2015-02-05 12:02:29 +0100

    Changes

--- Code/Testvideo.m
+++ Code/Testvideo.m
@@ -1,13 +1,42 @@
 p = 1;
 %73,74
-for(k = 700)
+results = {};
+for(k = 1:4:1901)
 frame=read(vid,k);
 kenteken = run(frame);
 if length(kenteken)>1
-    results{p,1} = kenteken;
-    results{p,2} = k;
-    time = toc;
-    results{p,3} = time; 
-    p = p + 1;
+%     if(isempty(results))
+        results{p,1} = kenteken;
+        results{p,2} = k;
+        time = toc;
+        results{p,3} = time; 
+        results{p,4} = 1;
+        p = p + 1;
+%     else
+%         if(~isempty(results) && length(results(:,1))>5)
+%             marge = 5;
+%         else
+%             marge = length(results(:,1));
+%         end
+%         a = 0;
+%         for(i = p-marge:p-1)
+%             if(sum(char(results(i,1)) == kenteken)==8)
+%                 amount = cell2mat(results(i,4)) + 1;
+%                 results(i,4) = num2cell(amount);
+%                 i=p-1;
+%                 a=1;
+%             end
+%         end
+%         if a == 0
+%             results{p,1} = kenteken;
+%             results{p,2} = k;
+%             time = toc;
+%             results{p,3} = time; 
+%             results{p,4} = 1;
+%             p = p + 1;
+%         end    
+%     end
+% end
 end
 end
+% results = results(:,1:3);
